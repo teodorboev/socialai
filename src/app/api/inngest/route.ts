0adf58047af89402f70be74945103e53bec3bde4
Teodor Boev <teodor.boev@me.com>
2026-03-01 09:03:48 +0000

    refactor: dynamically load all Inngest functions from the functions module.

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,22 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { contentPipeline, onUrgentTrend } from "@/inngest/functions/content-pipeline";
-import { publishScheduled } from "@/inngest/functions/publish-scheduled";
-import { engagementMonitor, onNewEngagement } from "@/inngest/functions/engagement-monitor";
-import { analyticsSnapshot, weeklyAnalyticsReport } from "@/inngest/functions/analytics-snapshot";
-import { monthlyStrategyPlanner, onOnboardingComplete } from "@/inngest/functions/strategy-planner";
+import * as functions from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    contentPipeline,
-    onUrgentTrend,
-    publishScheduled,
-    engagementMonitor,
-    onNewEngagement,
-    analyticsSnapshot,
-    weeklyAnalyticsReport,
-    monthlyStrategyPlanner,
-    onOnboardingComplete,
-  ],
+  functions: Object.values(functions),
 });
